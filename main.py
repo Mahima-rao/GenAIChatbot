15b1046527d0356bff146f8646ae78ef53211c59
Mahima <mahimaraok@gmail.com>
2025-06-25 19:13:36 +0100

    Commit with UI

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from chatbot.generative_bot import handle_user_input
-# from api import order_cancellation, order_tracking
-
-# app = FastAPI()
-
-# app.include_router(order_cancellation.router)
-# app.include_router(order_tracking.router)
-
-# class ChatInput(BaseModel):
-#     message: str
-
-# @app.post("/chat")
-# def chat(input: ChatInput):
-#     response = handle_user_input(input.message)
-#     return {"response": response}
-
 from fastapi import FastAPI
 from pydantic import BaseModel
 from agent.agent_core import agent_loop
